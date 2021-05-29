d9ca0690e9da001c77234771fcf7f34a59ab588f
yayxs <leaderywl@163.com>
2021-05-29 14:04:27 +0800

    build(change pm2 config): change pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'VastBlog',
+      name: 'vast-blog-web',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      instances: 2, // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
     },
